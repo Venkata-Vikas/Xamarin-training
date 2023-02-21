abc08b0c765c48fd67a828ff855abc8c5f4ecb81
Vikas <Vikas.gorrela@winwire.com>
2023-02-21 14:26:07 +0530

    Initial push

---
+++ SampleApp/SampleApp/QuotesPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SampleApp
+{
+    public partial class QuotesPage : ContentPage
+    {
+        public QuotesPage()
+        {
+            InitializeComponent();
+            string[] quotes =
+            {
+                "The greatest glory in living lies not in never falling, but in rising every time we fall."
+            };
+            label1.Text = quotes[0];
+        }
+    }
+}
+
